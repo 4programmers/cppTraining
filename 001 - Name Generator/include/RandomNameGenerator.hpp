9de45fdb3a0f6449558ab2431f5f2221d611b372
Patryk A. Wertka <spartanpageagain@gmail.com>
2014-10-19 03:22:59 +0200

    Added RandomNameGenerator header; The `RandomNameGenerator` class is prepared to be template.

---
+++ 001 - Name Generator/include/RandomNameGenerator.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <vector>
+#include <array>
+#include <string>
+#include <random>
+
+class RandomNameGenerator{
+public:
+    typedef std::vector<std::string> Prefixes;
+    typedef std::vector<std::string> Cores;
+    typedef std::vector<std::string> Sufixes;
+    typedef std::default_random_engine RandomEngine;
+    typedef std::string Result;
+public:
+    RandomNameGenerator(
+        const Prefixes &prefixes,
+        const Cores &cores,
+        const Sufixes &sufixes,
+        RandomEngine &re
+    ): _prefixes(prefixes), _cores(cores), _sufixes(sufixes), _re(re){}
+
+    Result operator()(
+        size_t minCoreRepeats,
+        size_t maxCoreRepeats
+    ){
+        Result result;
+        typedef std::uniform_int_distribution<int> distro;
+        enum{ PrefixID, CoreID, SufixID, CoreRepeatsID, Count /* <-- Have to be the last!*/};
+        std::array<distro, Count> distros = {
+            distro(0, _prefixes.size()-1),
+            distro(0, _cores.size()-1),
+            distro(0, _sufixes.size()-1),
+            distro(minCoreRepeats, maxCoreRepeats)
+        };
+        result += _prefixes[distros[PrefixID](_re)];
+        size_t coreRepeats = distros[CoreRepeatsID](_re);
+        while(coreRepeats--)
+            result += _cores[distros[CoreID](_re)];
+        return result += _sufixes[distros[SufixID](_re)];
+    }
+private:
+    Prefixes _prefixes;
+    Cores _cores;
+    Sufixes _sufixes;
+    RandomEngine &_re;
+};
